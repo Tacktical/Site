e92531e2c9cae7c07b10c209e4bc9b1f3ff99928
Jon Rowe <hello@jonrowe.co.uk>
2013-03-09 07:21:12 +1100

    middleman config

---
+++ config.rb
@@ -0,0 +1,14 @@
+#require 'susy'
+
+set :css_dir,    'stylesheets'
+set :js_dir,     'javascripts'
+set :images_dir, 'images'
+
+activate :sprockets
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :cache_buster
+  activate :smusher #compressed PNG files
+end
